1fd92b15898c2bcda26a853b8a407eb573cf205a
rochmisz <109878606+rochmisz@users.noreply.github.com>
2024-06-03 00:08:02 +0200

    file validation added

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,20 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Map<Character, Integer> characterIntegerMap = readData(new File("huffman.txt"));
-        for (Map.Entry<Character, Integer> entry : characterIntegerMap.entrySet()) {
-            MyPriorityQueue.add(entry.getValue(), entry.getKey());
-        }
-        if (!MyPriorityQueue.isQueueEmpty()) {
-            MyPriorityQueue.generateHuffmanCodes();
-            Map<Character, String> codes = new TreeMap<>(MyPriorityQueue.getCodes());
-            for (Map.Entry<Character, String> entry : codes.entrySet()) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
+        if (isFileValid(new File("huffman.txt"))){
+            Map<Character, Integer> characterIntegerMap = readData(new File("huffman.txt"));
+            for (Map.Entry<Character, Integer> entry : characterIntegerMap.entrySet()) {
+                MyPriorityQueue.add(entry.getValue(), entry.getKey());
             }
+            if (!MyPriorityQueue.isQueueEmpty()) {
+                MyPriorityQueue.generateHuffmanCodes();
+                Map<Character, String> codes = new TreeMap<>(MyPriorityQueue.getCodes());
+                for (Map.Entry<Character, String> entry : codes.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+            }
+        }else {
+            System.out.println("Invalid file");
         }
     }
     private static Map<Character,Integer> readData(File f) throws FileNotFoundException {
@@ -31,5 +35,28 @@ public class Main {
         }
         return data;
     }
-    
+    public static boolean isFileValid(File f) throws FileNotFoundException {
+        boolean valid = true;
+        Scanner s = new Scanner(f);
+        int i = 0;
+        int n = 0;
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            String[] split = line.split(" ");
+            if(i==0){
+                n = Integer.parseInt(split[0]);
+            }
+            if (split.length != 1 && i == 0 && !split[0].matches("[0-9]+")) {
+                return false;
+            }
+            if (split.length != 2 && i>0 && !split[0].matches("[a-zA-Z]+") && !split[1].matches("[0-9]+")){
+                return false;
+            }
+            i++;
+        }
+        if((i-1)!=n){
+            return false;
+        }
+        return valid;
+    }
 }
